88367a3fe5ae3faad1eb35eec8f05353e42fa5f9
Callum Greenwood <105824909+Ammattikar@users.noreply.github.com>
2023-12-05 00:52:47 -0600

    schedule: basic endpoints

--- src/schedule.rs
+++ src/schedule.rs
@@ -6,3 +6,16 @@ pub struct ScheduledDeparture {
 	train: TrainId,
 	time: std::time::SystemTime,
 }
+
+#[post("/create", data = "<schedule>")]
+pub fn create_schedule(mut schedule: RocketJson<ScheduledDeparture>, db: &State<Database>) {
+	let id = db.get_monotonic_id();
+	schedule.id = ScheduledDepartureId(id);
+
+	db.write_item(schedule.id, &schedule.0, TABLE_SCHEDULES);
+}
+
+#[get("/list")]
+pub fn list_schedules(db: &State<Database>) -> RocketJson<Vec<ScheduledDeparture>> {
+	RocketJson(db.read_all(TABLE_SCHEDULES).expect("failed to read schedules list"))
+}
