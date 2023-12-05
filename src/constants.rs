a7b45d13c31f47a5cf79d441455bdb6f49324017
Callum Greenwood <105824909+Ammattikar@users.noreply.github.com>
2023-12-05 00:51:39 -0600

    constants: More tables

--- src/constants.rs
+++ src/constants.rs
@@ -6,6 +6,10 @@ pub const TABLE_SEAT_CLASSES: &[u8] = b"seat_classes";
 
 pub const TABLE_DEPARTURES: &[u8] = b"departures";
 
+pub const TABLE_SCHEDULES: &[u8] = b"schedules";
+
+pub const TABLE_USERS: &[u8] = b"users";
+
 pub const TABLE_BIKEY_DEPARTURES_BY_TRAIN: &[u8] = b"departures_by_train";
 
 pub const TABLE_BIKEY_DEPARTURES_BY_TIME: &[u8] = b"departures_by_time";
